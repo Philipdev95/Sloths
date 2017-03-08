45c6b1e75efed631e8484d03b1c65964aa51877b
RobinJohannesson <robinjohannesson96@gmail.com>
2017-03-08 20:33:19 +0100

    API recept
    hittar bild, instruktioner och rubrik för recept(väljer första möjliga
    recept efter sökning)

--- script.js
+++ script.js
@@ -3,18 +3,33 @@ $("#searchButton").on("click", function () {
     $("#searchButton").html("Din mamma kan va en " + text + "!");
 });
 
-function recipe() {
+function recipe(searchq) {
     $.ajax({
         type: "GET",
-        url: "https://api.edamam.com/search?q=chicken",
+        url: "https://api.edamam.com/search?q=" + searchq + "",
         dataType: "json",
         error: function (response) {
             alert('Error: There was a problem processing your request, please refresh the browser and try again');
         },
         success: function (response) {
             console.log(response);
+            var i;
+            var list = "";
+            var x = response.hits[0].recipe.ingredientLines;
+            for (i = 0; i < x.length; i++){
+                list = "";
+                searchrecipe = response.hits[0].recipe.ingredientLines[i];
+                $("#recipe-text").append("<p>" + searchrecipe + "</p>");
+            }
+            searchlabel = response.hits[0].recipe.label;
+            $("#recipe-label").append(searchlabel);
+            searchimg = response.hits[0].recipe.image;
+            $("#recipe-img").attr("src", searchimg);
         }
     });
 };
 
-recipe()
+$("#provaq").on("click", function(){
+    searchq = $("#search").val();
+    recipe(searchq);
+});
