2f18b1fdc5801461126869560f053c631631ff4d
RobinJohannesson <robinjohannesson96@gmail.com>
2017-03-08 18:10:40 +0100

    lagt till API i script

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ $("#searchButton").on("click", function () {
     var text = $("#search").val()
     $("#searchButton").html("Din mamma kan va en " + text + "!");
 });
+
+function recipe() {
+    $.ajax({
+        type: "GET",
+        url: "https://api.edamam.com/search?q=chicken",
+        dataType: "json",
+        error: function (response) {
+            alert('Error: There was a problem processing your request, please refresh the browser and try again');
+        },
+        success: function (response) {
+            console.log(response);
+        }
+    });
+};
+
+recipe()
