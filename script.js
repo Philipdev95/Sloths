a1ea1c37624fcb9523ff58b4aeefe5ac2db65d50
Philipdev95 <philipdev@icloud.com>
2017-03-13 12:41:56 +0100

    letsTurn it up

--- script.js
+++ script.js
@@ -8,41 +8,47 @@ function recipe(searchq, searchAmount, searchHealth) {
         },
         success: function (response) {
             console.log(response);
-            var i;
-            var list = "";
-            var x = response.hits[0].recipe.ingredientLines;
-            for (i = 0; i < x.length; i++){
+            var i,
+                list = "",
+                x = response.hits[0].recipe.ingredientLines;
+            for (i = 0; i < x.length; i++) {
                 searchrecipe = response.hits[0].recipe.ingredientLines[i];
                 list += "<p>" + searchrecipe + "</p>";
             }
-			printRecipe(response, searchAmount, list)
+			printRecipe(response, searchAmount, list);
         }
     });
 }
 
 function printRecipe(response, searchAmount, list){
-	console.log(searchAmount)
+	console.log(searchAmount);
 	for(i = 0; i < searchAmount.length; i++){
-		$("#recipe-text").html(list);
-		searchlabel = response.hits[0].recipe.label;
-		$("#recipe-label").html(searchlabel);
-		searchimg = response.hits[0].recipe.image;
-		$("#recipe-img").attr("src", searchimg);
+		//$("#recipe-text").html(list);
+		searchlabel = response.hits[i].recipe.label;
+		//$("#recipe-label").html(searchlabel);
+		searchimg = response.hits[i].recipe.image;
+		//$("#recipe-img").attr("src", searchimg);
+        
+        $(".recipe").append(
+        "<div class='col-xs-12 recipe-divs media'> <div class='col-xs-4 media-right'> <a href='#'> <img class='col-xs-12 media-object recipe-img' src='" + searchimg + "' alt='img'> </a> </div> <div class='media-body'><h4 class='media-heading'>" + searchlabel + "</h4> <p>" + list + "</p> </div> </div>"
+                       );
+        alert(i);
 	}
+    console.log(i);
 }
 
 $("#searchhere").on("click", function () {
-    var searchHealth = health_label();
-    console.log(searchHealth);
-    var searchDiet = diet_label();
-    console.log(searchDiet);
-    var searchq = $("#search").val(),
+    var searchHealth = health_label(),
+        searchDiet = diet_label(),
+        searchq = $("#search").val(),
         searchAmount = $("#chooseamount option:selected").text();
+    console.log(searchHealth);
+    console.log(searchDiet);
     recipe(searchq, searchAmount, searchHealth);
 });
 
 function health_label() {
-    health_labels = $(".health_label:checked").map(function(){
+    health_labels = $(".health_label:checked").map(function () {
         return this.value;
     }).get().join('%20');
     if (health_labels == "") {
@@ -54,7 +60,7 @@ function health_label() {
 }
 
 function diet_label() {
-    diet_labels = $(".diet_label:checked").map(function(){
+    diet_labels = $(".diet_label:checked").map(function () {
         return this.value;
     }).get().join('%20');
     if (diet_labels == "") {
