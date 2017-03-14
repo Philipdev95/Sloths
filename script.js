ee7f468dc8c9279bb4d1449b994c3ed7ad4386a0
RobinJohannesson <robinjohannesson96@gmail.com>
2017-03-14 15:34:59 +0100

    ooopps

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function printRecipe(response, searchAmount, list) {
 function random(response, searchAmount){
 	var count = response.count;
 	console.log(count);
-	for var (i = searchAmount; i>0; i--){
+	for (var i = searchAmount; i>0; i--){
 		1 + Math.floor(Math.random() * count);
 	}
 }
