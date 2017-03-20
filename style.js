00da3bb8365d003f904cddbcd9fdbdcc96c07bbc
Philipdev95 <philipdev@icloud.com>
2017-03-20 18:26:25 +0100

    #loading centered
    det var pga att den var fixerad till att alltid vara 180px*180px. Har
    nu ändrat så att höjden matchar bredden.

--- style.js
+++ style.js
@@ -1,3 +1,11 @@
+//The function below makes 
+//the loading circle's height match its width.
+$(window).resize(function () {
+    var width = $("#loader").outerWidth();
+    $("#loader").css({height: width});
+});
+//The function below appears not to work...
+//but its not vital
 $("#searchhere").click(function () {
     $("#searchhere").css({boxShadow: "0px 0px 10px 0.5px #000"});
     $("#recipe").addClass("radi-back");
